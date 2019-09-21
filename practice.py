2b150fdf6ffe5a39d2ec77fbc5dfe4ff38c69bce
ldetu350 <55633896+ldetu350@users.noreply.github.com>
2019-09-21 15:01:21 -0400

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,44 @@
+'''
+Level 1: Create a program that asks the user for their favorite drink
+         and then prints that the computer loves the drink too.
+
+
+Example output:
+------------------------------------
+What is your favorite drink? Water
+I love Water too!
+'''
+# Your code here
+
+
+'''
+Level 2: Create a program that asks the user for their age and a year.
+         Print "In <year>, you will be <number> years old." where the 
+         number is your age in that year.
+
+Example output:
+------------------------------------
+How old are you? 19
+Give me a year: 2022
+You will be 22 years old in 2022.
+'''
+# Your code here
+age= input("how old are you")
+print("I am",age)
+'''
+Level 3: Create a program that asks the user for their name and then
+         prints their name in a frame of stars.
+
+Hint: You can make 6 stars by typing "*" * 6
+      Get the length of a string with len()
+      example: len("Keenan") = 6
+
+Example output:
+------------------------------------
+What is your name? Keenan
+**********
+* Keenan *
+**********
+'''
+
+# Your code here
